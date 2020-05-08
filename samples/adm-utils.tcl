28093cb585e68f1e9d039bfc326ff52b58c8ec6d
Kobayasi, Hiroaki <buribullet@gmail.com>
2020-05-08 16:02:01 +0900

    Put into a namespace

--- samples/adm-utils.tcl
+++ samples/adm-utils.tcl
@@ -1,9 +1,18 @@
 # -*- coding: utf-8; mode: tcl -*-
 
+namespace eval adm-utils {
+
 proc check-user {user} {
     file-has ^${user}: /etc/passwd
 }
 
 proc check-group {group} {
     file-has ^${group}: /etc/group
+}
+
+
+namespace export *
+
+namespace current
+
 }

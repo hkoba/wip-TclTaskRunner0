hkoba/wip-TclTaskRunner0

commit 910ec0ed766ca2db5cc16957917424a020ea127e

For better error reporting

lib/builder.tcl
@@ -284,7 +284,12 @@ snit::type ::TclTaskRunner::TaskSetBuilder {
         if {[$myRegistry exists $name]} {
             $myRegistry get $name
         } else {
-            $self taskset define file $fn -depth $depth
+            set rc [catch {
+                $self taskset define file $fn -depth $depth
+            } error]
+            if {$rc} {
+                error "Can't load tcltask $fn: $error"
+            }
         }
     }
 
@@ -347,7 +352,15 @@ snit::type ::TclTaskRunner::TaskSetBuilder {
             $self dputs $depth [$def cget -name] => [$def dump]
         }
 
-        $self taskset finalize $def {*}$args
+        set rc [catch {$self taskset finalize $def {*}$args} error]
+        if {$rc} {
+            if {$options(-debug)} {
+                error "Error found in [$def cget -name]: $error\n\
+                Dump: [$def dump]\n"
+            } else {
+                error $error
+            }
+        }
 
         if {$options(-debug) >= 3} {
             $self dputs $depth ==> [$def dump]

lib/tasksetdef.tcl
@@ -62,6 +62,9 @@ snit::type ::TclTaskRunner::TaskSetDefinition {
         if {$name eq ""} {
             set name $options(-default)
         }
+        if {![dict exists $myDeps $name]} {
+            error "Unknown target: $name\nKnown targets are: [dict keys $myDeps]"
+        }
         set dict [dict get $myDeps $name]
         list $self [dict get $dict kind] $name
     }

commit 6e0c9cceeabf906450909fd6eca19e7a838169fd

Fix `[$self use $target]`
* Broken in 910ec0ed766ca2db5cc16957917424a020ea127e

lib/builder.tcl
@@ -281,9 +281,7 @@ snit::type ::TclTaskRunner::TaskSetBuilder {
 
     method {taskset ensure-loaded} {fn {depth 0}} {
         set name [$myRegistry relative-name $fn]
-        if {[$myRegistry exists $name]} {
-            $myRegistry get $name
-        } else {
+        if {![$myRegistry exists $name]} {
             set rc [catch {
                 $self taskset define file $fn -depth $depth
             } error]
@@ -291,6 +289,7 @@ snit::type ::TclTaskRunner::TaskSetBuilder {
                 error "Can't load tcltask $fn: $error"
             }
         }
+        $myRegistry get $name
     }
 
     method {taskset define file} {origFn args} {
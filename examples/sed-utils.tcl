77283f0c87097af3d811429aa652da8ded60b476
Kobayasi, Hiroaki <hkoba@ssri.com>
2023-04-20 16:36:26 +0900

    Add dispatch-sed-tasks

--- examples/sed-utils.tcl
+++ examples/sed-utils.tcl
@@ -3,19 +3,48 @@
 namespace eval sed-utils {
     
     proc sed-insert-before {fn address content args} {
-        ** exec sed {*}$args "${address}i $content" $fn
+        ** exec sed {*}$args -e [task-insert-before $address $content] $fn
+    }
+
+    proc task-insert-before {address content} {
+        return "${address}i $content"
     }
 
     proc sed-append-after {fn address content args} {
-        ** exec sed {*}$args "${address}a $content" $fn
+        ** exec sed {*}$args -e [task-append-after  $address $content] $fn
+    }
+
+    proc task-append-after {address content} {
+        return "${address}a $content"
     }
 
     proc sed-delete-at {fn address args} {
-        ** exec sed {*}$args "${address}d" $fn
+        ** exec sed {*}$args -e [task-delete-at $address] $fn
+    }
+
+    proc task-delete-at address {
+        return "${address}d"
+    }
+
+    proc task-command {string} {
+        return $string
     }
 
     proc sed-apply-at {fn address command args} {
         ** exec sed {*}$args -e "$address $command" $fn
+    }
+
+    proc dispatch-sed-tasks {fn taskList args} {
+        set cmdList []
+        foreach task $taskList {
+            set rest [lassign $task op]
+            set cmdName [namespace current]::task-$op
+            if {[info commands $cmdName] eq ""} {
+                error "Unknown sed task: $task"
+            }
+            lappend cmdList -e [$cmdName {*}$rest]
+        }
+        ** exec sed {*}$args {*}$cmdList $fn
     }
 
     namespace export *
